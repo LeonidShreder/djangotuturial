9524c76369562370a695b56f19bfb1cf7a84a536
Leo Messi <leonidshreder@gmail.com>
2018-12-18 15:10:20 +0200

    del some

--- venv/Scripts/django-admin.py
+++
@@ -1,6 +0,0 @@
-#!C:\Users\Leo\PycharmProjects\djangotest\venv\Scripts\python.exe
-# EASY-INSTALL-DEV-SCRIPT: 'Django==2.2.dev20181217100344','django-admin.py'
-__requires__ = 'Django==2.2.dev20181217100344'
-__import__('pkg_resources').require('Django==2.2.dev20181217100344')
-__file__ = 'C:\\Users\\Leo\\PycharmProjects\\djangotest\\django\\django\\bin\\django-admin.py'
-exec(compile(open(__file__).read(), __file__, 'exec'))
